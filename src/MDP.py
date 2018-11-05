a497aafc3eed6675caed5a6d629fb0066800c713
upupming <upupming@gmail.com>
2018-11-05 12:50:45 +0800

    Added: caculation detail & more examples

--- src/MDP.py
+++ src/MDP.py
@@ -1,11 +1,20 @@
 import numpy
+import sys
 
-def markov_decision_process(o, g, h, s, T, f, p):
-    
+def markov_decision_process(o, g, h, s, T, f, p, test=False):
     def cal_transition_function():
         """
         Return P where P[before_sold][after_sold] is the probablity of given before_sold products at the begining of the month, there is after_sold products left at the end if the month. See table 1
         """
+        if test:
+            return numpy.array(
+                [
+                    [1, 0, 0, 0],
+                    [3/4, 1/4, 0, 0],
+                    [1/4, 1/2, 1/4, 0],
+                    [0, 1/4, 1/2, 1/4]
+                ]
+            )
         P = numpy.zeros((s+1, s+1))
         # First dimension is products amount before sold
         for before_sold in range(s+1):
@@ -27,6 +36,15 @@ def markov_decision_process(o, g, h, s, T, f, p):
         """
         Return R where R[current_stock][action] is the reward got if the decision maker take action `action` when current stock is `current_stock`. See table 2
         """
+        if test:
+            return numpy.array(
+                [
+                    [0, -1, -2, -5],
+                    [5, 0, -3, 0],
+                    [6, -1, 0, 0],
+                    [5, 0, 0, 0]
+                ]
+            )
 
         def F(stock):
             """
@@ -69,22 +87,27 @@ def markov_decision_process(o, g, h, s, T, f, p):
             u[T][stock] = g(T)
         # Then we begin to get u[T-1], u[T-2], ..., u[0]
         for t in range(T-1, -1, -1):
+            print('Calculating time:', t)
             # Second dimension is current stock
             for current_stock in range(s+1):
+                print('\t if current stock is:', current_stock)
                 best_action = 0
                 maximum_reward = 0
                 # For all actions, we calculate a cumulative reward, and then take the maxmimum, see equation (16)
-                for inbound in range(0, s+1):
+                for inbound in range(0, s+1-current_stock):
                     # reward from current_stock to next_stock
                     reward = R[current_stock][inbound]
                     # reward after current_stock
                     for next_stock in range(s+1):
-                        reward += P[current_stock][next_stock] * u[t+1][next_stock]
+                        reward += P[current_stock+inbound][next_stock] * u[t+1][next_stock]
+                    print('\t\t if take action:', inbound, 'the cumulative reward will be:', reward)
                     if reward > maximum_reward:
                         maximum_reward = reward
                         best_action = inbound
                 u[t][current_stock] = maximum_reward
                 a[t][current_stock] = best_action
+                print('\t so we choose action:', best_action, 'and will get max reward:', maximum_reward)
+            print('\n')
         return (u, a)
         
     (u, a) = cal_cumulative_reward()
@@ -93,7 +116,7 @@ def markov_decision_process(o, g, h, s, T, f, p):
 
 
 
-def run_example():
+def run_example(T=3, test=False):
     # Model parameters, see equation (13)
     def o(u):
         return 2*u;
@@ -104,11 +127,12 @@ def run_example():
     def f(u):
         return 8*u;
     s = 3
-    T = 3
     # p_0, p_1, ..., p_s-1, q_s
     # p_0, p_1, p_2, p_3 for this example
     p = [1/4, 1/2, 1/4, 0]
 
-    markov_decision_process(o, g, h, s, T, f, p)
+    markov_decision_process(o, g, h, s, T, f, p, test=test)
 
-run_example()
+
+print('T =', sys.argv[1], '\n')
+run_example(T=int(sys.argv[1]), test=(len(sys.argv)>=3 and (sys.argv[2] == 'test')))
